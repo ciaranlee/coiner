7a0177d0b9f07c88f244fc24eaefbf060374c49c
Ciaran Lee <ciaran.lee@gmail.com>
2013-11-30 20:48:05 +0000

    blank repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coiner::Application.config.secret_key_base = 'e094505e5a47b9a36abf03c679940184efacd35bb0b90d409821d9b02f048a8e3446011511e644a3bb1f7ca4ea0f2d2bc3e23775850996945f19c5daa0a5477c'
